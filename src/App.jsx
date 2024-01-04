74e497ce18509da6c266e724dac1ef943006a95c
Rabsickles91 <grewalrabian@gmail.com>
2024-01-04 11:28:30 -0500

    it worked!

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
+import { useEffect } from 'react'
+import { useState } from 'react'
 import './App.css'
 import WeatherData from './Components/WeatherData'
 
+let key = import.meta.env.VITE_API_KEY
+
+
 function App() {
+
+  let [weather, setWeather] = useState(null)
+
+  async function getData() {
+      
+      try { 
+          const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=Detroit,MI,USA&limit=5&appid=${key}`)
+          const data = await response.json()
+          console.log(data)
+          
+      } catch(error) {
+          console.log(error)
+      }
+  }
+
+  useEffect(()=>{getData()},[] )
+
 
   return (
     <>
--- src/Components/WeatherData.jsx
+++ src/Components/WeatherData.jsx
@@ -1,26 +1,8 @@
-import { useState } from "react";
-
-let key = import.meta.env.VITE_API_KEY 
 
 export default function WeatherData() {
-    let [weather, setWeather] = useState([])
-
-    async function getData(e) {
-        e.preventDefault()
-
-        try { 
-
-            const response = await fetch("")
-            const data = await response.json()
-            console.log(data)
-            
-        } catch(error) {
-            console.log(error)
-        }
-    }
 
   return (
 
-    <button onClick={()=>{getData}}>WeatherData</button>
+    <button className="weather-button">WeatherData</button>
   )
 }
