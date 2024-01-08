9c49cdc3bd89006d765adef56a4fb53d59bf5b15
Rabsickles91 <grewalrabian@gmail.com>
2024-01-07 19:46:56 -0500

    comment

--- src/Components/WeatherDisplay.jsx
+++ src/Components/WeatherDisplay.jsx
@@ -2,12 +2,12 @@ export default function WeatherDisplay({city, temperature, description}) {
     return (
       <div>
         <h1>{city}</h1>
-        {temperature !== null && (
-            <>
-                <h2>Current Temperature: {temperature}°F</h2>
-                <p>Description: {description} </p> 
-            </>
-        )}      
+            {temperature !== null && (
+                <>
+                    <h2>Current Temperature: {temperature}°F</h2>
+                    <p>Description: {description} </p> 
+                </>
+            )}      
       </div>
     );
   };
